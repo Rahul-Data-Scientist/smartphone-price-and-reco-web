11d05b317700d33344882632b441868cbd94076a
unknown <rahulraj24042003@gmail.com>
2025-06-17 04:43:42 +0530

    new commit

--- pages/1_Price_Prediction.py
+++ pages/1_Price_Prediction.py
@@ -7,6 +7,7 @@ from downloader import download_file_from_google_drive
 # Downloading price predictor pipeline from google drive
 download_file_from_google_drive("1SlGyFxIr2mndxyV4t4A3vG-jykPYYWOe", "price_predictor_pipeline.pkl")
 
+
 with open("price_predictor_data.pkl", "rb") as file:
     smartphones_df = pickle.load(file)
 
