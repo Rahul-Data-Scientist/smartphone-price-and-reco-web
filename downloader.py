7a4b2dcaecc733cf247a81e481e7ca11ac5c5f02
unknown <rahulraj24042003@gmail.com>
2025-06-17 04:43:08 +0530

    new commit

--- downloader.py
+++ downloader.py
@@ -1,11 +1,7 @@
-import os
 import gdown
 
 
 def download_file_from_google_drive(file_id, destination):
-    if not os.path.exists(destination):
-        print(f"Downloading {destination}...")
-        url = f"https://drive.google.com/uc?id={file_id}"
-        gdown.download(url, destination, quiet=False)
-    else:
-        print(f"{destination} already exists.")
+    # Construct the direct URL
+    url = f"https://drive.google.com/uc?id={file_id}"
+    gdown.download(url, destination, quiet=False, fuzzy=True)
